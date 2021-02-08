9e777612da748e225acf3bd9006a125ebe67c3a0
Leandro <leabertero@hotmail.com>
2021-02-08 19:04:39 -0300

    Promedio Alumnos

---
+++ Ejemplo Swift/Ejemplo Swift/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Ejemplo_Swift
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.Nota = new System.Windows.Forms.Label();
+            this.LblRes = new System.Windows.Forms.Label();
+            this.TxtNota = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.BackColor = System.Drawing.Color.Black;
+            this.BtnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAceptar.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.BtnAceptar.Location = new System.Drawing.Point(235, 126);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(174, 69);
+            this.BtnAceptar.TabIndex = 0;
+            this.BtnAceptar.Text = "VER PROMEDIO";
+            this.BtnAceptar.UseVisualStyleBackColor = false;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // Nota
+            // 
+            this.Nota.AutoSize = true;
+            this.Nota.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota.Location = new System.Drawing.Point(42, 87);
+            this.Nota.Name = "Nota";
+            this.Nota.Size = new System.Drawing.Size(158, 28);
+            this.Nota.TabIndex = 1;
+            this.Nota.Text = "Ingrese Nota:";
+            // 
+            // LblRes
+            // 
+            this.LblRes.AutoSize = true;
+            this.LblRes.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes.Location = new System.Drawing.Point(474, 146);
+            this.LblRes.Name = "LblRes";
+            this.LblRes.Size = new System.Drawing.Size(0, 28);
+            this.LblRes.TabIndex = 2;
+            // 
+            // TxtNota
+            // 
+            this.TxtNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNota.Location = new System.Drawing.Point(80, 142);
+            this.TxtNota.Name = "TxtNota";
+            this.TxtNota.Size = new System.Drawing.Size(80, 26);
+            this.TxtNota.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(683, 338);
+            this.Controls.Add(this.TxtNota);
+            this.Controls.Add(this.LblRes);
+            this.Controls.Add(this.Nota);
+            this.Controls.Add(this.BtnAceptar);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "EJEMPLO SWICH (SEGUN)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Label Nota;
+        private System.Windows.Forms.Label LblRes;
+        private System.Windows.Forms.TextBox TxtNota;
+    }
+}
+
---
+++ Ejemplo Swift/Ejemplo Swift/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo_Swift
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int Nota = System.Convert.ToInt32(TxtNota.Text);
+
+            switch (Nota)
+
+            {
+                case 1:
+                    LblRes.Text = "APLAZADO";
+                    break;
+
+                case 2:
+                    LblRes.Text = "APLAZADO";
+                    break;
+
+                case 3:
+                    LblRes.Text = "APROBADO";
+                    break;
+
+                case 4:
+                    LblRes.Text = "PROMOCIONADO";
+                    break;
+
+                case 5:
+                    LblRes.Text = "PROMOCIONADO";
+                    break;
+                default: 
+                    LblRes.Text = "Nota no valida";
+                    break; 
+
+                
+
+
+            }
+        }
+
+        
+    }
+}
