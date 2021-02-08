dc4590f5e1d68dcde8e5941c8a0e620bbc5d21c5
Leandro <leabertero@hotmail.com>
2021-02-07 23:42:39 -0300

    Formulario arranque

---
+++ PruebaIf/PruebaIf/FrmSiguiente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaIf
+{
+    public partial class FrmSiguiente : Form
+    {
+        public FrmSiguiente()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSiguiente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
