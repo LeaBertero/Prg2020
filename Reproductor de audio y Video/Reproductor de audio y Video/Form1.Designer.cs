eac2acec15819d44a63124cc40858c86ca4b2985
Leandro <leabertero@hotmail.com>
2021-02-15 23:51:32 -0300

    Reproductor de Audio

---
+++ Reproductor de audio y Video/Reproductor de audio y Video/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Reproductor_de_audio_y_Video
+{
+    partial class FormAudio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAudio));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.BtnCargar = new System.Windows.Forms.Button();
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.BtnPause = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.LblRuta = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(24, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(473, 323);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // BtnCargar
+            // 
+            this.BtnCargar.BackColor = System.Drawing.Color.Black;
+            this.BtnCargar.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCargar.ForeColor = System.Drawing.Color.Red;
+            this.BtnCargar.Location = new System.Drawing.Point(36, 375);
+            this.BtnCargar.Name = "BtnCargar";
+            this.BtnCargar.Size = new System.Drawing.Size(102, 64);
+            this.BtnCargar.TabIndex = 1;
+            this.BtnCargar.Text = "Cargar";
+            this.BtnCargar.UseVisualStyleBackColor = false;
+            this.BtnCargar.Click += new System.EventHandler(this.BtnCargar_Click);
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.BackColor = System.Drawing.Color.Black;
+            this.BtnPlay.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlay.ForeColor = System.Drawing.Color.Red;
+            this.BtnPlay.Location = new System.Drawing.Point(153, 375);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(102, 64);
+            this.BtnPlay.TabIndex = 2;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = false;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // BtnPause
+            // 
+            this.BtnPause.BackColor = System.Drawing.Color.Black;
+            this.BtnPause.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPause.ForeColor = System.Drawing.Color.Red;
+            this.BtnPause.Location = new System.Drawing.Point(272, 375);
+            this.BtnPause.Name = "BtnPause";
+            this.BtnPause.Size = new System.Drawing.Size(102, 64);
+            this.BtnPause.TabIndex = 3;
+            this.BtnPause.Text = "Puase";
+            this.BtnPause.UseVisualStyleBackColor = false;
+            this.BtnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.BackColor = System.Drawing.Color.Black;
+            this.BtnStop.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStop.ForeColor = System.Drawing.Color.Red;
+            this.BtnStop.Location = new System.Drawing.Point(395, 375);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(102, 64);
+            this.BtnStop.TabIndex = 4;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = false;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // LblRuta
+            // 
+            this.LblRuta.AutoSize = true;
+            this.LblRuta.Location = new System.Drawing.Point(33, 469);
+            this.LblRuta.Name = "LblRuta";
+            this.LblRuta.Size = new System.Drawing.Size(30, 13);
+            this.LblRuta.TabIndex = 5;
+            this.LblRuta.Text = "Ruta";
+            this.LblRuta.Click += new System.EventHandler(this.LblCargar_Click);
+            // 
+            // FormAudio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(861, 541);
+            this.Controls.Add(this.LblRuta);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnPause);
+            this.Controls.Add(this.BtnPlay);
+            this.Controls.Add(this.BtnCargar);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "FormAudio";
+            this.Text = "Reproductor de audio y Video";
+            this.Load += new System.EventHandler(this.FormAudio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button BtnCargar;
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Button BtnPause;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label LblRuta;
+    }
+}
+
---
+++ Reproductor de audio y Video/Reproductor de audio y Video/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reproductor_de_audio_y_Video
+{
+    public partial class FormAudio : Form
+    {
+        private string ruta = "";
+        public FormAudio()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormAudio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCargar_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                ruta = openFileDialog1.FileName;
+
+                LblRuta.Text = ruta;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = ruta;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void LblCargar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+    }
+}
